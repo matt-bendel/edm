9c21bad292e80c87d6cb759593029f02fad8326c
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-12-19 14:25:22 -0500

    Added debug plot to understand sampler behavior

--- generate.py
+++ generate.py
@@ -47,6 +47,7 @@ def edm_sampler(
     num_steps=100, sigma_min=0.002, sigma_max=80, rho=7,
     S_churn=0, S_min=0, S_max=float('inf'), S_noise=1,
 ):
+    print(num_steps)
     # Adjust noise levels based on what's supported by the network.
     sigma_min = max(sigma_min, net.sigma_min)
     sigma_max = min(sigma_max, net.sigma_max)
