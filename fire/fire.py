a544a663dc953db86528f8b8471d6f9300b86665
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-12-19 13:33:50 -0500

    Added debug plot to understand sampler behavior

--- fire/fire.py
+++ fire/fire.py
@@ -133,7 +133,7 @@ class FIRE:
             # 1. Denoising
             mu_2, eta = self.denoising(mu_1_noised, gamma_r)
             mu_2 = mu_2 + torch.randn_like(mu_2) / (eta[:, 0, None, None, None]).sqrt()
-            plt.imsave(f'tmp_fire_mu_2_{i}.png', clear_color(fire_out[0]))
+            plt.imsave(f'tmp_fire_mu_2_{i}.png', clear_color(mu_2[0]))
 
             tr_approx = 0.
             num_samps = 50
